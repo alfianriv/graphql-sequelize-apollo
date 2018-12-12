d2400bdd9efbf8b42a1e2688eb7f93fb81a2c4c7
Terence Jeong <terence.jeong@gmail.com>
2018-12-12 17:08:20 +1100

    added morePages param and hashed the end cursor

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -5,6 +5,10 @@ import {
   isMessageOwner,
 } from './authorization';
 
+const toCursorHash = string => Buffer.from(string).toString('base64');
+const fromCursorHash = string =>
+  Buffer.from(string, 'base64').toString('ascii');
+
 
 export default {
   Query: {
@@ -13,22 +17,33 @@ export default {
         ? {
           where: {
             createdAt: {
-              [Sequelize.Op.lt]: cursor,
+              [Sequelize.Op.lt]: fromCursorHash(cursor),
             },
           },
         }
         : {};
-      return await models.Message.findAll({
+      const messages = await models.Message.findAll({
         order: [['createdAt', 'DESC']],
-        limit,
+        limit: limit + 1,
         ...cursorOptions,
       });
-    },
-    message: async (parent, { id }, { models }) => {
-      return await models.Message.findById(id);
+      // will check if there is a next page or not.
+      const hasNextPage = messages.length > limit;
+
+      const edges = hasNextPage ? messages.slice(0, -1) : messages;
+
+      // pageInfo now has the cursor of th last message in the list.
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage,
+          endCursor: toCursorHash(
+            edges[edges.length - 1].createdAt.toString()
+          ),
+        },
+      }
     },
   },
-
   Mutation: {
     createMessage: combineResolvers(
       isAuthenticated,
