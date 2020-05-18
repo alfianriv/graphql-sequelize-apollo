6d1f7441077c0675f0dd27a3f82539266db0c975
alfianriv <alfian@femaledaily.com>
2020-05-18 20:11:06 +0700

    remove console.log

--- src/models/index.js
+++ src/models/index.js
@@ -3,15 +3,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-console.log([
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  },
-])
-
 let sequelize;
 
 sequelize = new Sequelize(
