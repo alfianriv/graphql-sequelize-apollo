8ea4261d8a1921567e8abaf135964757135dd519
alfianriv <alfian@femaledaily.com>
2020-05-18 20:09:46 +0700

    debug

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,16 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv'
 
-dotenv.config().parsed
+dotenv.config()
+
+console.log([
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+])
 
 let sequelize;
 
