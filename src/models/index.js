7b00ac64914a6527f629da2b580469f8b1e0e1b4
alfianriv <alfian@femaledaily.com>
2020-05-24 01:34:47 +0700

    support url connection

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,38 @@
-import Sequelize from 'sequelize';
-import dotenv from 'dotenv'
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 let sequelize;
 
-sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.URL, {
     dialect: process.env.DB_DIALECT,
-    operatorsAliases: false
-  },
-);
+    operatorsAliases: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      operatorsAliases: false,
+    }
+  );
+}
 
 const models = {
-  User: sequelize.import('./user'),
-  Donate: sequelize.import('./donate'),
+  User: sequelize.import("./user"),
+  Donate: sequelize.import("./donate"),
 };
 
-Object.keys(models).forEach(key => {
-  if ('associate' in models[key]) {
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
     models[key].associate(models);
   }
 });
 
-export {
-  sequelize
-};
+export { sequelize };
 export default models;
